552bec5fe27a7fcef05d2425ab349ed1f45b1cc4
hanahang <coco2020gmgm@gmail.com>
2026-05-29 19:46:32 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'stock-dashboard-v2';
+const urlsToCache = [
+  './',               // 기본 경로 (index.html)
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
